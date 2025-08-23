b895b8dad7d49f0b79d5c05be77c70db1db4bfa8
Rudra kumar sharma <rudrakumarsharma4213@gmail.com>
2025-08-23 18:39:42 +0530

    download-component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import UnicornScene from "unicornstudio-react";
 import DragNdrop from './components/DragNdrop';
-
+import TextInput from './components/TextInput';
 function App() {
 
   return (
@@ -12,13 +12,14 @@ function App() {
       <div className='App'>
       <div className='unicorn'><UnicornScene projectId="jQIQhaKLsJjU8cH6PMGw" width={100+"vw"} height={100+"vh"} /></div>
       <DragNdrop/>
+      <div className='flex flex-row space-x-4 items-center justify-center'>
+        <TextInput/>
         <div className='buttonContainer'>
-          <div className='pasteBox'></div>
           <div className='downloadButton'><button>Download</button></div>
-        </div>
+        </div></div>
       </div>
     </>
   )
 }
 
-export default App;
+export default App
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function TextInput() {
+  const [ID, setID] = useState("");
+
+  const handleChange = (event) => {
+    setID(event.target.value);
+  };
+
+  return (
+    <div className=" flex flex-col items-center justify-center bg-gray-100 p-6">
+
+      <input
+        type="text"
+        value={ID}
+        onChange={handleChange}
+        placeholder="Type something..."
+        className="w-72 px-4 py-2 border rounded-lg focus:outline-none border-gray-300"
+      />
+    </div>
+  );
+}
+
+export default TextInput
